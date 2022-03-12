5d62bcd9f9e474bc8b79349eb5d795d557ceb72f
Ed Flack <eflack3@ford.com>
2022-03-12 02:16:18 +0000

    Initial Commit

---
+++ boxing-app/boxing_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  boxing_appApp.swift
+//  boxing-app
+//
+//  Created by Flack, Edward (E.) on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct boxing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
